06a73bd3466afd9d6ea1b72bd5fd754939929b4b
Pawan <pawansharma93520@gmail.com>
2024-06-21 23:41:22 +0530

    user and admin register api ready to use

--- model/user_model.go
+++ model/user_model.go
@@ -3,10 +3,9 @@ package model
 type User struct {
 	UserID    int    `json:"user_id"`
 	FullName  string `json:"full_name"`
-	Username  string `json:"username"`
+	Username  string `json:"user_name"`
 	Email     string `json:"email"`
 	MobileNo  string `json:"mobile_no"`
 	Role      string `json:"role"`
 	CreatedAt string `json:"created_at"`
-	UpdatedAt string `json:"updated_at"`
 }
